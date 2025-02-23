ed7235b502ebcf5cf4dcc4e925b92d6920e0472b
Alex9600t <dukovaleks2@gmail.com>
2025-02-23 05:38:24 +0300

    feat(RickRoll)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 const infoBtn = document.getElementById('infoBtn');
 const infoModal = document.getElementById('infoModal');
 const closeModal = document.getElementById('closeModal');
+const rickBtn = document.getElementById('rickBtn');
+const rickModal = document.getElementById('rickModal');
+const closeRickModal = document.getElementById('closeRickModal');
+const linksBtn = document.getElementById('linksBtn');
+const linksModal = document.getElementById('linksModal');
+const closeLinksModal = document.getElementById('closeLinksModal');
 
-infoBtn.addEventListener('click', () => {
-    infoModal.classList.add('active');
+rickBtn.addEventListener('click', () => {
+    rickModal.classList.add('active');
 });
 
-closeModal.addEventListener('click', () => {
-    infoModal.classList.remove('active');
+closeRickModal.addEventListener('click', () => {
+    rickModal.classList.remove('active');
+});
+
+document.getElementById('RickRollBtn').addEventListener('click', () => {
+    document.getElementById('RickRollModal').classList.add('active');
+    rickModal.classList.remove('active');
+});
+
+linksBtn.addEventListener('click', () => {
+    linksModal.classList.add('active');
+});
+
+closeLinksModal.addEventListener('click', () => {
+    linksModal.classList.remove('active');
 });
